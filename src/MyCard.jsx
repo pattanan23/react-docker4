8ac6f3dea7e01240c9b5bb9b0ac66825db73d091
pattanan23 <151853833+pattanan23@users.noreply.github.com>
2025-03-22 20:14:26 +0700

    deletbutton

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -27,7 +27,6 @@ export default function MyCard(props) {
   </CardContent>
   <CardActions>
     <Button size="small">Learn More</Button>
-    <Button size="small">Learn More xxx</Button>
   </CardActions>
 </Card>
 
